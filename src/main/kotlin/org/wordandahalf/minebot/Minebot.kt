6ecc39ae015007b50011ce54328154d5fcc21a87
Ryan Jones <mail2ryanjones@gmail.com>
2020-08-22 19:33:37 -0400

    Fixed build process, 'maven install' works properly

--- src/main/kotlin/org/wordandahalf/minebot/Minebot.kt
+++ src/main/kotlin/org/wordandahalf/minebot/Minebot.kt
@@ -10,23 +10,24 @@ import kotlin.system.exitProcess
 object Minebot
 {
     lateinit var API : DiscordApi
-}
 
-fun main(args: Array<String>)
-{
-    if(args.size != 1)
+    @JvmStatic
+    fun main(args: Array<String>)
     {
-        System.err.println("Please ensure that your Discord bot token is the only command line parameter")
-        exitProcess(-1)
+        if(args.size != 1)
+        {
+            System.err.println("Please ensure that your Discord bot token is the only command line parameter")
+            exitProcess(-1)
+        }
+
+        Minebot.API = DiscordApiBuilder()
+                .setToken(args[0])
+                .addListener(MinebotCommandListener())
+                .addListener(MinebotLinkNetworkManager.MessageListener())
+                .login()
+                .join()
+
+        MinebotLinkManager.init()
+        MinebotLinkNetworkManager.start()
     }
-
-    Minebot.API = DiscordApiBuilder()
-        .setToken(args[0])
-        .addListener(MinebotCommandListener())
-        .addListener(MinebotLinkNetworkManager.MessageListener())
-        .login()
-        .join()
-
-    MinebotLinkManager.init()
-    MinebotLinkNetworkManager.start()
 }
